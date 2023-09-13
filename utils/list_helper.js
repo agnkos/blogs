980208b085d0d5bec54e8fd694288b09613811c7
agnkos <101097155+agnkos@users.noreply.github.com>
2023-09-13 18:58:36 +0200

    exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,8 +10,22 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = blogs => {
+    const reducer = (acc, curr) => {
+        return acc.likes > curr.likes ? acc : curr
+    }
+    if (blogs.length !== 0) {
+        const blog = blogs.reduce(reducer)
+        return {
+            title: blog.title,
+            author: blog.author,
+            likes: blog.likes,
+        }
+    } else return 'the blog list is empty'
+}
 
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog
 }
